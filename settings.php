cdf9e149c0ac388696a58a5dd1a088c5f03b5018
buckaroo-labs <46908086+buckaroo-labs@users.noreply.github.com>
2023-12-11 16:03:38 -0800

    Create settings.php

---
+++ settings.php
@@ -0,0 +1,16 @@
+<?php
+$settings['Timezone']='US/Eastern';
+$settings['BaseURI']='/dav/';
+$settings['Realm']='SabreDAV';
+
+/*The following settings should be overridden by 'settingsDB.php', 
+  and will be if the file exists and has this content.
+  (Create this file using the lines below as template).
+  The file 'settingsDB.php' is listed in .gitignore because 
+  it should contain the password and doesn't belong in a repo. */
+$settings['DBName']='sabredavdb';
+$settings['DBUser']='username';
+$settings['DBPass']='paSSw0rd';
+$settings['DBHost']='127.0.0.1';
+include(' settingsDB.php');
+?>
