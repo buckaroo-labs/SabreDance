4cf167fdfdc4641cd1cb987a551516516ee69db8
buckaroo-labs <46908086+buckaroo-labs@users.noreply.github.com>
2023-12-11 16:10:17 -0800

    Create server.php

---
+++ server.php
@@ -0,0 +1,22 @@
+<?php
+// https://sabre.io/dav/gettingstarted/
+use Sabre\DAV;
+require 'vendor/autoload.php';
+require 'settings.php';
+$rootDirectory = new DAV\FS\Directory('public');
+$server = new DAV\Server($rootDirectory);
+$server->setBaseUri($settings['BaseURI'] . 'server.php');
+$lockBackend = new DAV\Locks\Backend\File('data/locks');
+$lockPlugin = new DAV\Locks\Plugin($lockBackend);
+$server->addPlugin($lockPlugin);
+$server->addPlugin(new DAV\Browser\Plugin());
+
+use Sabre\DAV\Auth;
+$pdo = new \PDO('mysql:dbname=' . $settings['DBName'] . ';host=' . $settings['DBHost]' , $settings['DBUser]' , $settings['DBPass]' );
+$pdo->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+$authBackend = new Auth\Backend\PDO($pdo);
+$authBackend->setRealm( $settings['Realm]' );
+$authPlugin = new Auth\Plugin($authBackend);
+$server->addPlugin($authPlugin);
+$server->exec();
+?>
