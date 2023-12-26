2a7fe462b89b41acc4125a1ee1886541c1107d66
buckaroo-labs <46908086+buckaroo-labs@users.noreply.github.com>
2023-12-25 20:11:18 -0800

    Update batch_processing.php

--- batch_processing.php
+++ batch_processing.php
@@ -1,7 +1,6 @@
 <?php 
 //This file is meant to be called from the command line
 require_once "settingsHydrogen.php";
-require_once "settingsPasswords.php";
 require_once "Hydrogen/libDebug.php";
 require_once "Hydrogen/clsDataSource.php";
 require_once "clsDB.php";
