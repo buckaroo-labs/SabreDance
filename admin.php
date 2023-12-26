29fbc8ae97e1d6cfc991af7b67c552721d464b8e
buckaroo-labs <46908086+buckaroo-labs@users.noreply.github.com>
2023-12-26 11:34:24 -0800

    Update admin.php

--- admin.php
+++ admin.php
@@ -1,78 +1,13 @@
 <?php
-header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
-header("Cache-Control: post-check=0, pre-check=0", false);
-header("Pragma: no-cache");
-
+$pagetitle="Administration";
+include "Hydrogen/pgTemplate.php";
 require('settings.php');
 $authFailureText='This page requires authentication.';
 $authFailureText2='Invalid username/password. This page requires authentication.';
 
 $realm = $settings['Realm'];
-//https://www.php.net/manual/en/features.http-auth.php
-//
-//The info in this line will be replaced by a DB check
-//$users = array('admin' => 'mypass', 'guest' => 'goaway');
+
 $conn=mysqli_connect($settings['DBHost'],$settings['DBUser'],$settings['DBPass'],$settings['DBName']) ;
-$sql="select max(digesta1) as a1, count(*) as rowCount from users where username=?";
-
-if (empty($_SERVER['PHP_AUTH_DIGEST'])) {
-    header('HTTP/1.1 401 Unauthorized');
-    header('WWW-Authenticate: Digest realm="'.$realm.
-           '",qop="auth",nonce="'.uniqid().'",opaque="'.md5($realm).'"');
-
-    die($authFailureText);
-}
-
-
-// analyze the PHP_AUTH_DIGEST variable
-if (!($data = http_digest_parse($_SERVER['PHP_AUTH_DIGEST']))) die($authFailureText2);
-
-//Also die if username is invalid
-$stmt=$conn->prepare($sql);
-if ( false===$stmt ) die('prepare() failed: ' . htmlspecialchars($conn->error));
-$rc = $stmt->bind_param("s",$data['username']);
-if ( false===$rc ) die('bind_param() failed: ' . htmlspecialchars($stmt->error));
-
-$stmt->execute();
-$result = $stmt->get_result();
-$row = $result->fetch_array(MYSQLI_ASSOC);
-
-$A1=$row['a1'];
-$userCount=$row['rowCount'];
-if ($userCount==0) die ($authFailureText2);
-$stmt->close();
-
-//
-// generate the valid response
-// the following line is the value that will match the hash in the database
-//$A1 = md5($data['username'] . ':' . $realm . ':' . $users[$data['username']]);
-$A2 = md5($_SERVER['REQUEST_METHOD'].':'.$data['uri']);
-$valid_response = md5($A1.':'.$data['nonce'].':'.$data['nc'].':'.$data['cnonce'].':'.$data['qop'].':'.$A2);
-
-if ($data['response'] != $valid_response) die($authFailureText2);
-
-// ok, valid username & password
-echo 'You are logged in as: ' . $data['username'];
-//echo '<BR>' . $_SERVER['PHP_AUTH_DIGEST'];
-
-
-// function to parse the http auth header
-function http_digest_parse($txt)
-{
-    // protect against missing data
-    $needed_parts = array('nonce'=>1, 'nc'=>1, 'cnonce'=>1, 'qop'=>1, 'username'=>1, 'uri'=>1, 'response'=>1);
-    $data = array();
-    $keys = implode('|', array_keys($needed_parts));
-
-    preg_match_all('@(' . $keys . ')=(?:([\'"])([^\2]+?)\2|([^\s,]+))@', $txt, $matches, PREG_SET_ORDER);
-
-    foreach ($matches as $m) {
-        $data[$m[1]] = $m[3] ? $m[3] : $m[4];
-        unset($needed_parts[$m[1]]);
-    }
-
-    return $needed_parts ? false : $data;
-}
 
 function isPOSTedClean($postVar){
 	//allow only alphanumeric, '_', '-', '.'; '@' for email only; space (only for display name)
@@ -137,14 +72,8 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 	}
 }
 ?>
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta http-equiv="content-type" content="text/html; charset=UTF-8">
-    <meta name="viewport" content="width=device-width,initial-scale=1">
-    <meta name="format-detection" content="telephone=no">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>SabreDance</title>
+
+
     <style>
 	html,body,h1,h2,h3,h4,h5,h6 {font-family: "Roboto", sans-serif;}
 
@@ -162,32 +91,25 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 	}
 
     </style>
-    </head>
-    <body>
-    <H1>SabreDance: Admin page</H1>
 
-<?php
-ini_set(opcache.enable,0);
 
-	include 'settings.php';
+<!-- Main content: shift it to the right by 250 pixels when the sidebar is visible -->
+<div class="w3-main" style="margin-left:250px">
 
-		$mysqli=mysqli_connect($settings['DBHost'],$settings['DBUser'],$settings['DBPass'],$settings['DBName']) ;
-/*
+  <div class="w3-row w3-padding-64">
+    <div class="w3-twothird w3-container">
 
-	$result = $mysqli->query($sql) or die ("Failed to get result for SQL " . $sql .' '. mysqli_error());
-	while ($row=$result->fetch_assoc()) {
-	$row_out='<tr><td>' . implode('</td><td>',$row) . '</td></tr>';
-	echo $row_out . "\n";
-	}
+        <?php include "Hydrogen/pgLogin.php"; ?>
 
-*/
+    </div>
+    <div class="w3-third w3-container">
 
-	echo '<ul><li><a target="_blank" href="server.php">server.php</a></li>
+	<ul><li><a target="_blank" href="server.php">server.php</a></li>
 	<li><a target="_blank" href="calendarserver.php">calendarserver.php</a></li>
 	<li><a target="_blank" href="addressbookserver.php">addressbookserver.php</a></li>';
-	if (file_exists("Hydrogen")) echo('<li><a target="_blank" href="reminders.php">reminders.php</a></li>') ;
-	echo '</ul>';
-
+	<li><a target="_blank" href="reminders.php">reminders.php</a></li>
+	</ul>
+<?php 
 echo '<h2>Database contents</h2>';
 
 		echo '<h3>Registered users:</h3><table id="users" class="sortable"><tr><th>ID</th><th>Name</th></tr>';
@@ -211,5 +133,15 @@ echo '<h2>Database contents</h2>';
 		include 'userForm.php';
 
 ?>
-   </body>
-</html>
+   
+    </div>
+  </div>
+
+</div>
+
+<?php include "Hydrogen/elemFooter.php"; include "Hydrogen/elemNavbar.php";?>
+</body></html>
+
+
+
+
