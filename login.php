c91af665ff368a826c56a3a85e951dbca53cd91a
buckaroo-labs <46908086+buckaroo-labs@users.noreply.github.com>
2023-12-26 11:06:42 -0800

    Update login.php

--- login.php
+++ login.php
@@ -9,25 +9,18 @@ include "Hydrogen/pgTemplate.php";
   <div class="w3-row w3-padding-64">
     <div class="w3-twothird w3-container">
 
-	<?php include "Hydrogen/pgLogin.php"; ?>
+        <?php include "Hydrogen/pgLogin.php"; ?>
 
     </div>
     <div class="w3-third w3-container">
-        <ul>
-        <li><a target="_blank" href="server.php">server.php</a></li>
-<li><a target="_blank" href="calendarserver.php">calendarserver.php</a></li>
-<li><a target="_blank" href="addressbookserver.php">addressbookserver.php</a></li>
-<li><a target="_blank" href="reminders.php">reminders.php</a></li>
-</ul>
 
     </div>
   </div>
 
 </div>
 
-<?php include "Hydrogen/elemFooter.php"; ?>
+<?php include "Hydrogen/elemFooter.php"; include "Hydrogen/elemNavbar.php";?>
 </body></html>
-
 
 
 
