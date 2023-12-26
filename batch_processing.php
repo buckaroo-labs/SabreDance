9d039d4a86a58632bfc27cb7d9d09f4e0ff404f8
buckaroo-labs <46908086+buckaroo-labs@users.noreply.github.com>
2023-12-25 20:44:41 -0800

    Update batch_processing.php

--- batch_processing.php
+++ batch_processing.php
@@ -4,7 +4,7 @@ require_once "settingsHydrogen.php";
 require_once "Hydrogen/libDebug.php";
 require_once "Hydrogen/clsDataSource.php";
 require_once "clsDB.php";
-require_once "caldav-client.php";
+//require_once "caldav-client.php";
 require_once "clsReminder.php";			
 require_once "clsCalDAV.php";
 
