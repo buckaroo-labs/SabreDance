0cff23655618fe24ed019170c13c11a93ca053ec
buckaroo-labs <46908086+buckaroo-labs@users.noreply.github.com>
2023-12-26 16:18:06 -0800

    Update userForm.php
    add fields first/last name

--- userForm.php
+++ userForm.php
@@ -4,6 +4,8 @@
 <form id="newuser" action="admin.php" method="post">
 Username: <input type="text" name="davuser" ><br>
 Password: <input type="text" name="davpass" ><br>
+First Name: <input type="text" name="davfname" ><br>
+Last Name: <input type="text" name="davlname" ><br>
 Display Name: <input type="text" name="davname" ><br>
 email: <input type="email" name="davmail" ><br>
 <input type="submit" name="submit" value="New user">  
