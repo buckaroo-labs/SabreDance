f7f7049192a85095eaaf9ea9195085b7baf80248
buckaroo-labs <46908086+buckaroo-labs@users.noreply.github.com>
2023-12-26 16:27:04 -0800

    Update admin.php

--- admin.php
+++ admin.php
@@ -60,7 +60,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && $_SESSION['username']=='admin') {
 		$lname='blank';
 		$fname='blank';
 		if (isset($_POST['davfname'])) $fname=$_POST['davfname'] ;
-		if (isset($_POST['davlname'])) $fname=$_POST['davlname'] ;
+		if (isset($_POST['davlname'])) $lname=$_POST['davlname'] ;
                 $hash= password_hash($_POST['davpass'],PASSWORD_BCRYPT);
                 $sql1="INSERT INTO user (username,password_hash,email,first_name,last_name) 
 		VALUES ('" . $username ."','" . $hash . "','" . $_POST['davmail'] ."','" . $fname . "','" . $lname . "');";
