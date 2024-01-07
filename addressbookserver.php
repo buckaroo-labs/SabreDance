bf421d2d3d205eaf01532564db5f5634b99bb553
buckaroo-labs <46908086+buckaroo-labs@users.noreply.github.com>
2024-01-07 10:17:08 -0800

    Update addressbookserver.php

--- addressbookserver.php
+++ addressbookserver.php
@@ -16,8 +16,8 @@ require_once 'vendor/autoload.php';
 
 // Backends
 //This one works fine in the browser, but not with WinSCP
-//$authBackend = new Sabre\DAV\Auth\Backend\BasicCallBack('authenticate');
-$authBackend = new Sabre\DAV\Auth\Backend\PDO($pdo);
+$authBackend = new Sabre\DAV\Auth\Backend\BasicCallBack('authenticate');
+//$authBackend = new Sabre\DAV\Auth\Backend\PDO($pdo);
 $principalBackend = new Sabre\DAVACL\PrincipalBackend\PDO($pdo);
 $carddavBackend = new Sabre\CardDAV\Backend\PDO($pdo);
 $authBackend->setRealm($settings['Realm']);
